22b80b1681f6286348a76feef30474ae86d34ebd
jmendozacr <mendoza077@gmail.com>
2019-09-12 21:10:36 -0600

    setup project and adding form component

---
+++ src/components/Form.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+function Form() {
+  const [appointment, setAppointment] = useState({
+    pet: "",
+    owner: "",
+    date: "",
+    time: "",
+    description: "",
+  });
+
+  const handleChange = (e) => {
+    // console.log(e.target);
+    setAppointment({
+      ...appointment,
+      [e.target.name]: e.target.value
+    });
+  }
+  
+  console.log(appointment);
+
+  return (
+    <>
+      <h2>create appointment</h2>
+
+      <form>
+        <label>Pet Name</label>
+        <input
+          type="text"
+          name="pet"
+          className="u-full-width"
+          placeholder="Pet Name"
+          onChange={handleChange}
+        />
+
+        <label>Owner Name</label>
+        <input
+          type="text"
+          name="owner"
+          className="u-full-width"
+          placeholder="Owner Name"
+          onChange={handleChange}
+        />
+
+        <label>Date</label>
+        <input type="date" className="u-full-width" name="date" onChange={handleChange} />
+
+        <label>Time</label>
+        <input type="time" className="u-full-width" name="time" onChange={handleChange} />
+
+        <label>Symptom</label>
+        <textarea className="u-full-width" name="description" onChange={handleChange}></textarea>
+
+        <button type="submit" className="button-primary u-full-width">
+          Agregar
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default Form;
